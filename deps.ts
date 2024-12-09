af03e712f9b161d7d7df241c3a3ae31c1f09d2c6
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-12-10 06:37:46 +0900

    refactor: replace std/flags with std/cli

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export { parse as parseArgs } from "https://deno.land/std@0.166.0/flags/mod.ts";
+export { parseArgs } from "https://deno.land/std@0.207.0/cli/mod.ts";
 export { expandGlob } from "https://deno.land/std@0.166.0/fs/mod.ts";
 export * as colors from "https://deno.land/std@0.166.0/fmt/colors.ts";
 export type { Writer } from "https://deno.land/std@0.212.0/io/types.ts";
